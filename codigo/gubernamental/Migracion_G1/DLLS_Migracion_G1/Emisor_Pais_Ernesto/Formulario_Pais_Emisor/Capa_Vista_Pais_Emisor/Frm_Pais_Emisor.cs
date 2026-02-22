fa8c1b111f4b47e23a071aea05a614772d46470b
ErnestoSamayoa <esamayoaj1@miumg.edu.gt>
2026-02-22 12:47:57 -0600

    Subiendo Mi carpeta con formulario Pais Emisor Modificado

--- codigo/gubernamental/Migracion_G1/DLLS_Migracion_G1/Emisor_Pais_Ernesto/Formulario_Pais_Emisor/Capa_Vista_Pais_Emisor/Frm_Pais_Emisor.cs
+++ codigo/gubernamental/Migracion_G1/DLLS_Migracion_G1/Emisor_Pais_Ernesto/Formulario_Pais_Emisor/Capa_Vista_Pais_Emisor/Frm_Pais_Emisor.cs
@@ -7,14 +7,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//Ernesto Samayoa 0901-22-3415
 namespace Capa_Vista_Pais_Emisor
 {
     public partial class Frm_Pais_Emisor : Form
     {
         public Frm_Pais_Emisor()
         {
-            InitializeComponent();
+            {
+                InitializeComponent();
+                //parametros para navegador
+                Capa_Controlador_Navegador.Cls_ConfiguracionDataGridView config = new Capa_Controlador_Navegador.Cls_ConfiguracionDataGridView
+                {
+                    Ancho = 1100,
+                    Alto = 200,
+                    PosX = 10,
+                    PosY = 300,
+                    ColorFondo = Color.AliceBlue,
+                    TipoScrollBars = ScrollBars.Both,
+                    Nombre = "dgv_empleados"
+                };
+
+                string[] columnas = {
+                "tbl_pais_emisor",
+                "Pk_Id_Pais_Emisor",
+                "Cmp_Nombre_Pais",
+                "Cmp_Codigo_Pais"
+            };
+
+                string[] sEtiquetas = {
+                "Id Pais Emisor",
+                "Nombre Pais",
+                "Codigo Pais"
+            };
+
+
+
+                int id_aplicacion = 312;
+                navegador1.IPkId_Modulo = 4;
+                navegador1.IPkId_Aplicacion = id_aplicacion;
+                navegador1.configurarDataGridView(config);
+                navegador1.SNombreTabla = columnas[0];
+                navegador1.SAlias = columnas;
+                navegador1.SEtiquetas = sEtiquetas;
+                navegador1.mostrarDatos();
+            }
+
         }
     }
 }
--- codigo/gubernamental/Migracion_G1/DLLS_Migracion_G1/Emisor_Pais_Ernesto/ProyectoEjecucionP_E/ProyectoEjecucion/Properties/AssemblyInfo.cs
+++ codigo/gubernamental/Migracion_G1/DLLS_Migracion_G1/Emisor_Pais_Ernesto/ProyectoEjecucionP_E/ProyectoEjecucion/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("Capa_Vista_Pais_Emisor")]
+[assembly: AssemblyTitle("ProyectoEjecucion")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Capa_Vista_Pais_Emisor")]
-[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyProduct("ProyectoEjecucion")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("06bb28bb-8f93-4e71-a840-045467c86010")]
+[assembly: Guid("5f48cadd-a1e1-44da-832f-5c083eee8bb5")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
