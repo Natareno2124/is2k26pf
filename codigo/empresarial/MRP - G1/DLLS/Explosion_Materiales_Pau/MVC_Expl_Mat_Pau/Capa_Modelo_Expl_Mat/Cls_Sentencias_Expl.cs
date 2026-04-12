5c44cbd6df36337b55eb35e97ede1161b6eb0eea
IngPau <paulaleonardo289@gmail.com>
2026-04-12 14:29:21 -0600

    Creación de MVC para la Explosión de Materiales

---
+++ codigo/empresarial/MRP - G1/DLLS/Explosion_Materiales_Pau/MVC_Expl_Mat_Pau/Capa_Modelo_Expl_Mat/Cls_Sentencias_Expl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Modelo_Expl_Mat
+{
+    class Cls_Sentencias_Expl
+    {
+    }
+}
