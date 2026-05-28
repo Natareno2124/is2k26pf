48a6400ba0a77833167d95f7ab007025fd5d4ed2
Ruben Armando Lopez Luch <rlopezl24@miumg.edu.gt>
2026-05-27 22:08:23 -0600

    docker api

--- codigo/empresarial/MRP - G1/API_MRP_Orden_Recibida/API_MRP_Orden_Recibida/Program.cs
+++ codigo/empresarial/MRP - G1/API_MRP_Orden_Recibida/API_MRP_Orden_Recibida/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Controllers + Newtonsoft (CORRECTO)
+// Controllers + Newtonsoft
 builder.Services.AddControllers()
     .AddNewtonsoftJson();
 
@@ -20,13 +20,15 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+// Swagger habilitado siempre
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API_MRP_Orden_Recibida v1");
+});
 
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run("http://0.0.0.0:5000");
